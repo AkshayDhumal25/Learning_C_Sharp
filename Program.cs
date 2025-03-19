fe40013f576c66357d8ed483518f0cb83cd96c4b
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-19 12:24:09 +0530

    1- Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)

--- Program.cs
+++ Program.cs
@@ -86,18 +86,85 @@
 
 
 
+//using System;
+//using System.Diagnostics.Contracts;
+//namespace Conditionals
+//{
+//    class Program
+//    {
+//        public enum Season
+//        {
+//            Spring,
+//            Summer,
+//            Autumn,
+//            Winter
+//        }
+//        static void Main(string[] args)
+//        {
+//            //int hour = 19;
+//            //if(hour >0  && hour < 12)
+//            //{
+//            //    Console.WriteLine("It's morning.");
+//            //}else if(hour >= 12 && hour < 14)
+//            //{
+//            //    Console.WriteLine("It's afternoon.");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("It's a evening.");
+//            //}
+
+//            //bool isGoldCustomer = true;
+//            //float price;
+//            //if (isGoldCustomer)
+//            //{
+//            //    price = 19.95f;
+//            //}
+//            //else
+//            //{
+//            //    price = 29.95f;
+//            //}
+//            //float price = (isGoldCustomer) ? 19.95f : 29.95f;
+//            //Console.WriteLine(price);
+
+//            //var season = Season.Autumn;
+
+//            //switch (season)
+//            //{
+//            //    case Season.Autumn:
+//            //        Console.WriteLine("It's autumn and a beautiful");
+//            //        break;
+//            //    case Season.Summer:
+//            //        Console.WriteLine("It's perfect to go to beach.");
+//            //        break;
+//            //    default:
+//            //        Console.WriteLine("I don't understand that season!");
+//            //        break;
+//            //}
+//        }
+//    }
+//}
+
+//********************************Exercise********************************
 using System;
- namespace Conditionals
+namespace number
 {
-    class Program
+    class checkNum
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int hour = 10;
-            if(hour >0  && hour < 12)
+            int n = 0;
+            Console.WriteLine("Enter the number between 1 to 10 :");
+            n = Convert.ToInt32(Console.ReadLine());
+            if(n > 0 && n <= 10)
             {
-                Console.WriteLine("It's morning.");
+                Console.WriteLine("Valid");
             }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
         }
     }
 }
