96c0325b51cffafb9ebbb733669582cd4cbb41dd
Nimap <Nimap@DESKTOP-B6SDR2O>
2025-03-19 09:25:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learning
+{
+    class learn
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Akshay");
+        }
+    }
+}
